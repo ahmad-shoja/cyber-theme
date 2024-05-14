4c2ab9858cbcbbf866921fefd3cdaae97f71cb79
Ahmad <ahmad.shoja.999@gmail.com>
2024-05-14 05:09:37 +0330

    ❄ Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        gray: {
+          light: "#ededed",
+          base: "#f6f6f6",
+          dark: "#fafafa",
+          disabled: "#d5d5d5",
+        },
+        typography: {
+          title: "#000000",
+          title_reverse: "#ffffff",
+          subtitle: "#545454",
+          subdued: "#a4a4a4",
+          disabled: "#d5d5d5",
+        }
+
+
+      },
+    },
   },
   plugins: [],
 }
